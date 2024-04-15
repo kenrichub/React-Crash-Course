c63fdbfbda4e8a63807c065297dbbffd9be4a0ac
Kenric <kenric.chui@gmail.com>
2024-04-15 23:09:43 +1000

    push

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,7 +1,13 @@
-import Link from 'react-router-dom';
+import Link, { useParams } from 'react-router-dom';
+import { useState } from 'react';
 
 function Users () {
+    const {id} = useParams()
+    const [user, setUser] = useState({})
 
+    async function fetchUser() {
+        const { data } = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+    }
 }
 
 export default Users;
